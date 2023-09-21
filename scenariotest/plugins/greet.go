7ff4a35bf295734c34e163bbc0bffd4e037f932b
matumoto1234 <matumotosizuoka@gmail.com>
2023-09-21 21:04:55 +0900

    色々追加、修正
    - buf.yamlやbuf.gen.yamlを追加
    - シナリオテスト追加
      - scenariotest/以下
    - gen/以下にコード生成
    - protoファイルを修正
      - Statusの値の命名
      - GreetServiceをToDoServiceに修正

---
+++ scenariotest/plugins/greet.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	todov1 "github.com/matumoto1234/connect-go-todo/gen/todo/v1"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+var conn *grpc.ClientConn
+
+func newConn(target string) *grpc.ClientConn {
+	if conn != nil {
+		return conn
+	}
+
+	conn, err := grpc.Dial(
+		target,
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithBlock(),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	return conn
+}
+
+func NewGreetServiceClient() todov1.ToDoServiceClient {
+	conn := newConn("localhost:8080")
+	return todov1.NewToDoServiceClient(conn)
+}
